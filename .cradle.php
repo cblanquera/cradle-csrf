81a4c40352b57672499495ed35515dfb764c7f8b
Christian Blanquera <cblanquera@openovate.com>
2016-11-21 12:07:00 +0800

    added events

--- .cradle.php
+++ .cradle.php
@@ -12,3 +12,7 @@ Flow::register('csrf', function() use ($cradle) {
 
     return $cache;
 });
+
+$cradle->on('csrf-load', Flow::csrf()->load);
+$cradle->on('csrf-render', Flow::csrf()->render);
+$cradle->on('csrf-validate', Flow::csrf()->check);
